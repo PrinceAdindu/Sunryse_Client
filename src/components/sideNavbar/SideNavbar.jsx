93f97720b49da859e436efa941b8630a0c918854
hjob-1 <hjob12tadele@gmail.com>
2024-04-07 13:50:20 -0400

    refactor: separate sidenav constant values and created  a CreateSidelink component

--- src/components/sideNavbar/SideNavbar.jsx
+++ src/components/sideNavbar/SideNavbar.jsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import HomeIcon from '@mui/icons-material/Home';
-import ChairIcon from '@mui/icons-material/Chair';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import SettingsIcon from '@mui/icons-material/Settings';
-import SellIcon from '@mui/icons-material/Sell';
-import CampaignIcon from '@mui/icons-material/Campaign';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
-import NavTerraLogo from '../../assets/TerraLogoLong.png';
-import SunryseLogo from '../../assets/NewSunryseLogoTallNameFill.png';
-import { SIDE_NAV_OPTIONS, getCurrentTab } from './sideNavbarHelper';
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import ChairIcon from "@mui/icons-material/Chair";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import SettingsIcon from "@mui/icons-material/Settings";
+import SellIcon from "@mui/icons-material/Sell";
+import CampaignIcon from "@mui/icons-material/Campaign";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import NavTerraLogo from "../../assets/TerraLogoLong.png";
+import SunryseLogo from "../../assets/NewSunryseLogoTallNameFill.png";
+import { SIDE_NAV_OPTIONS, getCurrentTab } from "./sideNavbarHelper";
 
-import styles from './SideNavbar.module.scss';
-import config from '../../config';
+import styles from "./SideNavbar.module.scss";
+import config from "../../config";
 
 export default function SideNavbar() {
   const navigate = useNavigate();
@@ -29,46 +29,46 @@ export default function SideNavbar() {
 
   const showNav = !config.externalRoutes.includes(location.pathname);
 
-  const SideNavs = () => {
-    const sideBarDatas = [
-      {
-        link: SIDE_NAV_OPTIONS.HOME,
-        Icon: HomeIcon,
+  const sideBarDatas = [
+    {
+      link: SIDE_NAV_OPTIONS.HOME,
+      Icon: HomeIcon,
+    },
+    {
+      link: SIDE_NAV_OPTIONS.BOOKINGS,
+      Icon: ChairIcon,
+    },
+    {
+      link: SIDE_NAV_OPTIONS.BUSINESS_HOURS,
+      Icon: CalendarMonthIcon,
+    },
+    {
+      link: SIDE_NAV_OPTIONS.SERVICES,
+      Icon: SellIcon,
+    },
+    {
+      link: SIDE_NAV_OPTIONS.FINANCES,
+      Icon: AccountBalanceIcon,
+    },
+    {
+      link: SIDE_NAV_OPTIONS.MARKETING,
+      Icon: CampaignIcon,
+      IconProps: {
+        fontSize: "medium",
       },
-      {
-        link: SIDE_NAV_OPTIONS.BOOKINGS,
-        Icon: ChairIcon,
-      },
-      {
-        link: SIDE_NAV_OPTIONS.BUSINESS_HOURS,
-        Icon: CalendarMonthIcon,
-      },
-      {
-        link: SIDE_NAV_OPTIONS.SERVICES,
-        Icon: SellIcon,
-      },
-      {
-        link: SIDE_NAV_OPTIONS.FINANCES,
-        Icon: AccountBalanceIcon,
-      },
-      {
-        link: SIDE_NAV_OPTIONS.MARKETING,
-        Icon: CampaignIcon,
-        IconProps: {
-          fontSize: 'medium',
-        },
-      },
-      {
-        link: SIDE_NAV_OPTIONS.SUPPORT,
-        Icon: FavoriteBorderIcon,
-      },
-      {
-        link: SIDE_NAV_OPTIONS.SETTINGS,
-        Icon: SettingsIcon,
-      },
-    ];
+    },
+    {
+      link: SIDE_NAV_OPTIONS.SUPPORT,
+      Icon: FavoriteBorderIcon,
+    },
+    {
+      link: SIDE_NAV_OPTIONS.SETTINGS,
+      Icon: SettingsIcon,
+    },
+  ];
 
-    return sideBarDatas.map((sideBar) => {
+  const CreateSideLinks = ({ items }) =>
+    items.map((sideBar) => {
       const {
         Icon,
         link: { url, label },
@@ -80,7 +80,7 @@ export default function SideNavbar() {
           onClick={() => navigate(url)}
         >
           <Icon
-            fontSize='small'
+            fontSize="small"
             {...(sideBar.IconProps && sideBar.IconProps)}
             className={selected ? styles.iconSelected : styles.icon}
           />
@@ -90,13 +90,12 @@ export default function SideNavbar() {
         </div>
       );
     });
-  };
 
   return (
     showNav && (
       <div className={styles.sideNavcontainer}>
         <img src={SunryseLogo} className={styles.logoImage} />
-        <SideNavs />
+        <CreateSideLinks items={sideBarDatas} />
       </div>
     )
   );
