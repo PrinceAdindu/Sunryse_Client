17c7c19c6cbb8c0872c38fde24b709f1b5c1555e
hjob-1 <hjob12tadele@gmail.com>
2024-04-07 21:05:01 -0400

    refactor: SIDE_NAV_DATA constant moved into sidenavbarhelper file

--- src/components/sideNavbar/SideNavbar.jsx
+++ src/components/sideNavbar/SideNavbar.jsx
@@ -1,20 +1,9 @@
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import HomeIcon from "@mui/icons-material/Home";
-import ChairIcon from "@mui/icons-material/Chair";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import SettingsIcon from "@mui/icons-material/Settings";
-import SellIcon from "@mui/icons-material/Sell";
-import CampaignIcon from "@mui/icons-material/Campaign";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-import NavTerraLogo from "../../assets/TerraLogoLong.png";
-import SunryseLogo from "../../assets/NewSunryseLogoTallNameFill.png";
-import { SIDE_NAV_OPTIONS, getCurrentTab } from "./sideNavbarHelper";
-
-import styles from "./SideNavbar.module.scss";
-import config from "../../config";
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import SunryseLogo from '../../assets/NewSunryseLogoTallNameFill.png';
+import { getCurrentTab, SIDE_NAV_DATA } from './sideNavbarHelper';
+import styles from './SideNavbar.module.scss';
+import config from '../../config';
 
 export default function SideNavbar() {
   const navigate = useNavigate();
@@ -29,46 +18,8 @@ export default function SideNavbar() {
 
   const showNav = !config.externalRoutes.includes(location.pathname);
 
-  const sideBarDatas = [
-    {
-      link: SIDE_NAV_OPTIONS.HOME,
-      Icon: HomeIcon,
-    },
-    {
-      link: SIDE_NAV_OPTIONS.BOOKINGS,
-      Icon: ChairIcon,
-    },
-    {
-      link: SIDE_NAV_OPTIONS.BUSINESS_HOURS,
-      Icon: CalendarMonthIcon,
-    },
-    {
-      link: SIDE_NAV_OPTIONS.SERVICES,
-      Icon: SellIcon,
-    },
-    {
-      link: SIDE_NAV_OPTIONS.FINANCES,
-      Icon: AccountBalanceIcon,
-    },
-    {
-      link: SIDE_NAV_OPTIONS.MARKETING,
-      Icon: CampaignIcon,
-      IconProps: {
-        fontSize: "medium",
-      },
-    },
-    {
-      link: SIDE_NAV_OPTIONS.SUPPORT,
-      Icon: FavoriteBorderIcon,
-    },
-    {
-      link: SIDE_NAV_OPTIONS.SETTINGS,
-      Icon: SettingsIcon,
-    },
-  ];
-
-  const CreateSideLinks = ({ items }) =>
-    items.map((sideBar) => {
+  const SideNavOptions = () =>
+    SIDE_NAV_DATA.map((sideBar) => {
       const {
         Icon,
         link: { url, label },
@@ -95,7 +46,7 @@ export default function SideNavbar() {
     showNav && (
       <div className={styles.sideNavcontainer}>
         <img src={SunryseLogo} className={styles.logoImage} />
-        <CreateSideLinks items={sideBarDatas} />
+        <SideNavOptions />
       </div>
     )
   );
