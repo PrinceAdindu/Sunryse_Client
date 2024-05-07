96151853b84e328b3a1196ed0ada4f869054f5ea
hjob-1 <hjob12tadele@gmail.com>
2024-05-07 11:36:31 -0400

    refactor: external links array list removed from config file

--- src/config.js
+++ src/config.js
@@ -1,12 +1,5 @@
 const config = {
   apiUrl: 'http://localhost:5007',
-  externalRoutes: [
-    '/login',
-    '/register',
-    '/otp',
-    '/resetPassword',
-    '/resetPassword/email',
-  ],
 };
 
 export default config;
