0a6578a4cd96db2fbe0330a6f032048864155c8a
Tanraj <tanrajsd@gmail.com>
2024-08-26 15:34:32 -0600

    Added checkBox and removed OtpVerificationHelper for case sens

---
+++ src/components/checkbox/Checkbox.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import Checkbox from "@mui/material/Checkbox";
+
+CheckBox.propTypes = {
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func,
+};
+
+export default function CheckBox({checked, onChange = () => {}}) {
+  return (
+    <Checkbox
+      checked={checked}
+      onChange={(event) => onChange(event)}
+      sx={{
+        color: "#ff7073",
+        "&.Mui-checked": {
+          color: "#ff7073",
+        },
+      }}
+    />
+  );
+}
