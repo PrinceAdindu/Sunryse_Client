e0756d62d849459d776932d08024a8647bac9214
Tanraj <tanrajsd@gmail.com>
2024-12-14 12:27:03 -0700

    Remove old Navbar code

--- src/components/navbar/navbarHelpers.js
+++ src/components/navbar/navbarHelpers.js
@@ -8,21 +8,3 @@ export default async function logout(axios, navigate, toast) {
     }
   }
 }
-
-export async function getClinicName(axios, toast) {
-  try {
-    const fields = ["account.email"];
-    const res = await axios.get("/clinic", {
-      params: {
-        fields,
-      },
-    });
-
-    return res.data.account.email;
-  } catch (error) {
-    if (error?.response?.status === 400 || error?.response?.status === 500)
-      toast.error(
-        "There was an error loading your clinic data, please try again."
-      );
-  }
-}
